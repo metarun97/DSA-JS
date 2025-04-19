048f755da2b9d5c3fd9d7d6c017b70c2573646b8
Tarun Upadhyay <116960857+metarun97@users.noreply.github.com>
2025-04-19 21:50:29 +0530

    DSA Script file added

--- script.js
+++ script.js
@@ -2284,19 +2284,5 @@
 // console.log(resOfSortThePeople);
 
 
-class BankAccount {
-      #balance = 0; // private field
-    
-      deposit(amount) {
-        if (amount > 0) this.#balance += amount;
-      }
-    
-      getBalance() {
-        return this.#balance;
-      }
-    }
-    
-    const acc = new BankAccount();
-    acc.deposit(1000);
-    console.log(acc.getBalance()); // 1000
-    
+
+    console.log("Hello world")
